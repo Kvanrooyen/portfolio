ad9933d12fa648608ccc8ebeb3136e43b5a85398
Keagan van Rooyen <governor.keagan@outlook.com>
2021-02-12 23:18:53 +0000

    Styled home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,19 @@
-import React from "react"
+import React from "react";
+import image from "../michael-dziedzic-unsplash.jpg";
 
-export default function Home(){
-    return <h1>Home Page!</h1>
+export default function Home() {
+  return (
+    <main>
+      <img
+        src={image}
+        alt='Sphere with prism'
+        className='absolute object-cover w-full h-full'
+      />
+      <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+        <h1 className='text-6xl text-green-100 font-bold leading-none lg:leading-snug home-name'>
+          Hey! I'm Keagan
+        </h1>
+      </section>
+    </main>
+  );
 }

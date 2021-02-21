6cf4218b8a8ce8a305e1e34ea98b376b84e32a32
Keagan Van Rooyen <krooyen@fb.com>
2021-02-21 16:02:12 +0000

    Starting work on responsive design

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,11 @@ export default function Home() {
       <img
         src={image}
         alt='Sphere with prism'
-        className='absolute object-cover w-full h-full'
+        className='absolute object-cover w-screen h-full'
       />
-      <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
-        <h1 className='text-6xl text-green-100 font-bold leading-none lg:leading-snug home-name'>
-          Hey! I'm Keagan
+      <section className='relative flex justify-center min-h-screen pt-12 sm:pt-24 md:pt-36 lg:pt-48 px-8'>
+        <h1 className='text-3xl md:text-6xl lg:text-9xl text-green-100 font-bold leading-none lg:leading-snug '>
+          Hey, I'm Keagan!
         </h1>
       </section>
     </main>
